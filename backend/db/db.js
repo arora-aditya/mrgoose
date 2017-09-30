8ca42c48121b3e8bd35f2bae6d5d13827802d8cd
IbrahimIrfan <1ibrahimirfan@gmail.com>
2017-09-30 14:35:34 -0400

    Added more functions to database module

--- backend/db/db.js
+++ backend/db/db.js
@@ -4,19 +4,56 @@ const MONGO_URL = 'mongodb://localhost:27017/waterloop_internal';
 
 let mod = module.exports = {};
 
-//mod.createUser();
-
-function insertObj = (obj, collection){
+function insertObj(obj, collection){
   mongo.connect(MONGO_URL, function (err, db) {
       if (!err) {
-          console.log("Connected correctly to db");
+        console.log("Connected correctly to db");
+        db.collection(collection).insertOne(obj, function(err, res) {
+          if (err) throw err;
+          console.log("Inserted 1 object to " + collection);
+        });
+        db.close();
       }
-      db.collection(collection).insertOne(obj, function(err, res) {
-         if (err) throw err;
-         console.log("Inserted 1 object to " + collection);
-      });
-      db.close();
   });
+}
+
+function getAll(collection){
+  let promise = new Promise((resolve, reject) => {
+    mongo.connect(MONGO_URL, function (err, db) {
+        if (!err) {
+          console.log("Connected correctly to db");
+          db.collection(collection).find({}).toArray(function(err, result) {
+            if (err) throw err;
+            resolve(toObject(result));
+          });
+          db.close();
+        }
+    });
+  });
+  return promise;
+}
+
+function getObjById(query, collection){
+  let promise = new Promise((resolve, reject) => {
+    mongo.connect(MONGO_URL, function (err, db) {
+        if (!err) {
+            console.log("Connected correctly to db");
+        }
+        db.collection(collection).find({}).toArray(function(err, result) {
+            if (err) throw err;
+            res = toObject(result);
+
+            for (var i = 0; i < res.length; i++){
+              if (res[i]["_id"] == parseInt(query)){
+                resolve(res[i]);
+              }
+            }
+        });
+
+        db.close();
+    });
+  });
+  return promise;
 }
 
 module.createUser = (user) => {
@@ -25,4 +62,20 @@ module.createUser = (user) => {
 
 module.createSubteam = (team) => {
   insertObj(team, "team");
+}
+
+module.getUsers = () => {
+  return getAll("users");
+}
+
+module.getUserById = (id) => {
+  return getObjById(id, "users");
+}
+
+module.getSubteams = () => {
+  return getAll("teams");
+}
+
+module.getSubteamById = (id) => {
+  return getObjById(id, "teams");
 }
