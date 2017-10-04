b80c4676f9340957e2b90a06253647a002f7f03d
Aditya Arora <arora-aditya@users.noreply.github.com>
2017-10-04 04:26:32 -0400

    Add styling to row

--- front-end/app/components/SlackTableRow.js
+++ front-end/app/components/SlackTableRow.js
@@ -1,19 +1,32 @@
 import React from 'react';
 
+const style = {
+  display: 'inline-block',
+  fontFamily: 'monospace',
+  fontSize: 20,
+  textAlign: 'center',
+  color: 'white',
+  margin: '0px 0px 0px 0px',
+  padding: '0% 5% 0% 5%',
+  border: '1px solid #BA55D3'
+};
+
 const SlackTableRow = (props) => (
   <div className="flex">
-    <div className="Image">
+    <div style={style} className="Image">
       <img
+        height="20"
+        width="20"
         src = {props.usr.url}
       />
     </div>
-    <div className="Name">
+    <div style={style} className="Name">
       <p>{props.usr.Name}</p>
     </div>
-    <div className="Role">
+    <div style={style} className="Role">
       <p>{props.usr.Role}</p>
     </div>
-    <div className="Functions">
+    <div style={style} className="Functions">
       <a href={props.api.kick}><p>Kick</p></a>
     </div>
   </div>
