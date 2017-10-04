114e3d8f268bdeeee712f7021a33ac943deeb977
Aditya Arora <arora-aditya@users.noreply.github.com>
2017-10-04 04:27:28 -0400

    Add feed from array to support dynamically add elements

--- front-end/app/containers/SlackTable.js
+++ front-end/app/containers/SlackTable.js
@@ -1,23 +1,46 @@
 import React, { Component } from 'react';
 import SlackTableRow from '../components/SlackTableRow';
 
-const usrName = 'aditya';
-const usrRole = 'Software';
-const usrApi = '?';
-
 export default class SlackTable extends Component {
+  constructor() {
+    super();
+    this.members = [{
+      '_id': '59d406ca56549426b127ea07',
+      'username': '1234',
+      'fullName': 'Test User',
+      'email': 'test1@abc.ca',
+      'bio': 'WOW',
+      '__v': 0,
+      'teams': [],
+      'joinDate': '2017-10-03T21:53:04.120Z'
+    },
+    {
+      '_id': '59d406ca56549426b127eeerro07',
+      'username': '1234',
+      'fullName': 'Test User',
+      'email': 'test1@abc.ca',
+      'bio': 'WOW',
+      '__v': 0,
+      'teams': [],
+      'joinDate': '2017-10-03T21:53:04.120Z'
+    }
+    ];
+  }
   render() {
     return (
-      <SlackTableRow
+      <div>
+      {this.members.map(member => <SlackTableRow
         usr= {{
           url: 'imgur.com',
-          Name: usrName,
-          Role: usrRole,
+          Name: member.fullName,
+          Role: member.bio,
         }}
         api={{
-          kick: usrApi,
+          kick: member._id,
         }}
-      />
+       key = {member._id}/>)
+      }
+      </div>
     );
   }
 }
