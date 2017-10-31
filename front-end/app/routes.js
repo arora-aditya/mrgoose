4068edfae42a67642075c550932437d107ba655e
Aditya Arora <10962267+arora-aditya@users.noreply.github.com>
2017-10-30 22:46:04 -0400

    Deprecated repository

--- front-end/app/routes.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import { Route, Switch } from 'react-router-dom';
-import SlackTable from './containers/SlackTable';
-
-export default (
-  <Switch>
-    <Route exact path="/" component={SlackTable} />
-  </Switch>
-);
